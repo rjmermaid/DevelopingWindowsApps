f004a915cf21d8aad5831abd23a7a7b615991dd8
rjmermaid <bashenovjr@gmail.com>
2021-05-27 04:46:11 +0600

    Chapter06 TRYOUTS

---
+++ Chapter06/Ch06Ex01/Ch06Ex01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ch06Ex01
+{
+    class Program
+    {
+        static void Write()
+        {
+            //Console.WriteLine("Text output from function.");
+
+            Console.WriteLine("myString = {0}", myString);
+        }
+        static void Main(string[] args)
+        {
+            string myString = "String defined in Main()";
+            Write();
+            Console.ReadKey();
+        }
+    }
+}
