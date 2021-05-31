4eb149e1b2bf3fbf23ef254a94a7ecb5b5fdbf1c
rjmermaid <bashenovjr@gmail.com>
2021-06-01 04:51:56 +0600

    Chapter TRYOUTS

---
+++ Chapter09/Ch09Ex01/Ch09Ex01/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Ch09Ex01
+{
+    public abstract class myBase
+    {
+
+    }
+
+    internal class MyClass : myBase
+    {
+
+    }
+
+    public interface IMyBaseInterface
+    {
+
+    }
+
+    public interface IMyBaseInterface2
+    {
+
+    }
+
+    internal interface IMyInterface : IMyBaseInterface, IMyBaseInterface2
+    {
+
+    }
+
+    internal sealed class MyComplexClass : MyClass, IMyInterface
+    {
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyComplexClass myObj1 = new MyComplexClass();
+            Console.WriteLine(myObj1.ToString());
+            Console.ReadKey();
+        }
+    }
+}
