e65f7453c014db7cbb608fc6bf511831f3ff28dd
rjmermaid <bashenovjr@gmail.com>
2021-06-01 04:52:45 +0600

    Chapter09 TryOuts

---
+++ Chapter09/Ch09Ex03/Ch09Ex03/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ch09Ex03
+{
+    class myClass
+    {
+        public int val;
+    }
+
+    struct myStruct
+    {
+        public int val;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myClass objectA = new myClass();
+            myClass objectB = objectA;
+            objectA.val = 10;
+            objectB.val = 20;
+            myStruct structA = new myStruct();
+            myStruct structB = structA;
+            structA.val = 30;
+            structB.val = 40;
+            Console.WriteLine("objectA.val = {0}", objectA.val);
+            Console.WriteLine("objectB.val = {0}", objectB.val);
+            Console.WriteLine("structA.val = {0}", structA.val);
+            Console.WriteLine("structB.val = {0}", structB.val);
+            Console.ReadKey();
+        }
+    }
+}
